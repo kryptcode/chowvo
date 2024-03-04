c8dafaad1c293e6c91bb8598c68abfbf1eb8af0a
Koye <54333700+kryptcode@users.noreply.github.com>
2024-03-04 11:20:46 +0100

    "client and backend setup"

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<span>looka </span>} />
+        <Route path='/user-profile' element={<span>profile page </span>} />
+        <Route path='*' element={<Navigate to={'/'} />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes

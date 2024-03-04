f1fdd755f64a507aab6fe2f47001e75aab5b8ad6
Koye <54333700+kryptcode@users.noreply.github.com>
2024-03-04 11:54:53 +0100

    "landing layout"

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom";
-import './index.css'
+import './globals.css'
 import AppRoutes from './AppRoutes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
